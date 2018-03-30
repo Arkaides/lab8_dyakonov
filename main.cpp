ce90e09fc750d12155abf7ed80b8a1362fd077ed
Arkady <apkaides.7@gmail.com>
2018-03-31 02:42:00 +0900

    vse

--- main.cpp
+++ main.cpp
@@ -2,64 +2,44 @@
 #include <dirent.h>
 #include <sys/types.h>
 #include <sys/stat.h>
-#include <errno.h>
 #include <string.h>
 
-
 using namespace std;
+long int razmer, kol, kol1;
 
-int dcheck(char *dname)
-{
-    struct stat rs;
-    struct dirent *res;
-    DIR *dp;
+void dfs(){
+DIR* dp = opendir(".");
+	struct dirent *res;
 
-    char *cur_ent = malloc(sizeof(char));
-    int count = 0;
+	while ((res = readdir(dp)) != NULL) {
 
-    if ((dp = opendir(dname)) != NULL)
-    {
-        while((res = readdir(dp)) != NULL)
-        {
-           if(!strcmp(res->d_name, ".") || !strcmp(res->d_name, ".."))
-                continue;
-            strcpy(cur_ent, dname);
-            strcat(cur_ent,"/");
-           strcat(cur_ent, res->d_name);
-           stat(cur_ent, &rs);
-           if(!((rs.st_mode & S_IFMT) == S_IFDIR))
-            {
-             count+= rs.st_size;
-            }
-        }
-       closedir(dp);
-    }
-    return count;
-}
-
-int main(void)
-{
-    int a=0;
-    DIR* dp=opendir(".");
-    struct dirent *res;
-    struct stat rs;
-    while ((res=readdir(dp))!=NULL){
-        if(!strcmp(res->d_name,".")|| !strcmp(res->d_name,".."))
+        if (strcmp(res->d_name,".")==0 || strcmp(res->d_name,"..")==0)
             continue;
-        stat(res->d_name, &rs);
-        if((rs.st_mode&S_IFMT)==S_IFDIR)
-        {
-            count+=dcheck(res->d_name);
-        }
-        if(!((rs.st_mode&S_IFMT)==S_IFDIR))
-        {
-            count+=rs.st_size;
-        }
-        }
-        }
+        struct stat info;
+        stat(res->d_name, &info);
 
+    	if (S_ISDIR(info.st_mode)){
+            cout << " DIR" <<  "\n";
+            kol1++;
+            chdir(res->d_name);
+            dfs();
+            chdir("..");
+            }
+    	else{
+            razmer+= info.st_size;
+            kol++;
+    	}
+        cout << info.st_size<< " " << res->d_name << "\n";
+	}
 
     closedir(dp);
-    cout << count << "\n";
+    }
+
+
+int main()
+{
+	dfs();
+    cout << "file = " << kol << ", dir = " << kol1 << ", bytes = " << razmer << "\n";
     return 0;
 }
+
